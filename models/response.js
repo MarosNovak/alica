76e78104b32f90ea703a4e1b5522702e74ea14a4
Maros Novák <novkmaros@gmail.com>
2017-02-27 21:23:16 +0100

    Jira response, standup response functions.

--- models/response.js
+++ models/response.js
@@ -12,10 +12,6 @@ NEWSCHEMA('Response').make(function(schema) {
         return callback(buildResponseIssues(issues));
     });
 
-    schema.addOperation('issueResponse', function (error, model, issue, callback) {
-        return callback(buildSingleIssue(issue));
-    });
-
     schema.addOperation('enableModuleResponse', function (error, model, options, callback) {
         return callback(buildEnableModuleResponse(options));
     });
@@ -24,26 +20,35 @@ NEWSCHEMA('Response').make(function(schema) {
         return callback(buildBasicResponse(message));
     });
 
+    schema.addOperation('usersIssuesResponse', function (error, model, usersIssues, callback) {
+        return callback(buildResponseIssues(usersIssues));
+    });
+
+    /**
+     * Build USer's daily standup response from answers
+     * @param {Object} options - { user, answers }
+     */
+    schema.addOperation('userStandupAnswerResponse', function (error, model, options, callback) {
+        return callback(buildStandupResponse(options));
+    });
+
     function buildResponseIssues(issues) {
+        console.log('RESPONSEE', issues);
         var self = this;
         var json = {
             attachments: []
         };
-        if (issues && issues.contents) {
-            issues.contents.completedIssues.forEach(function(issue) {
-                var object = buildFieldsForIssue(issue);
-                json.attachments.push(object);
-            });
-            issues.contents.issuesNotCompletedInCurrentSprint.forEach(function(issue) {
-                var object = buildFieldsForIssue(issue);
-                json.attachments.push(object);
-            });
-        }
+        issues.forEach(function (issue) {
+            var object = buildFieldsForIssue(issue);
+            json.attachments.push(object);
+        });
+        console.log('JAAASON', json);
+
         return json;
     }
 
     function colorForIssue(issue) {
-        switch (issue.status.name) {
+        switch (issue.status) {
             case 'Done':
                 return 'good';
             case 'To Do':
@@ -58,16 +63,16 @@ NEWSCHEMA('Response').make(function(schema) {
     function buildFieldsForIssue(issue) {
         var self = this;
         var object = {
-            title:issue.summary,
-            title_link:buildIssueLink(issue.key),
-            color:colorForIssue(issue),
+            title: issue.title,
+            title_link: buildIssueLink(issue.key),
+            color: colorForIssue(issue),
             fields: [{
                 title: 'Assignee',
-                value: issue.assigneeName ? issue.assigneeName : 'Not assigned',
+                value: issue.assignee,
                 short: true
             }, {
                 title: 'Status',
-                value: issue.status.name,
+                value: issue.status,
                 short: true
             }]
         }
@@ -79,49 +84,29 @@ NEWSCHEMA('Response').make(function(schema) {
         return `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}${base}${issueKey}`;
     }
 
-    function buildSingleIssue(issue) {
-        var self = this;
-        var json = {
-            attachments: []
-        };
-        if (issue && issue.fields) {
-            var object = {
-                title:issue.fields.summary,
-                title_link:buildIssueLink(issue.key),
-                fields: [{
-                    title: 'Assignee',
-                    value: issue.fields.assignee ? issue.fields.assignee : 'Not assigned',
-                    short: true
-                }, {
-                    title: 'Status',
-                    value: issue.fields.status.name,
-                    short: true
-                }]
-            }
-            json.attachments.push(object);
-        }
-        return json;
-    }
-
     function buildHelp() {
         var title = 'This is a few commands I understand:\n\n';
         var general = '*_General Commands_*\n';
         var status = '`status` give you current modules status.\n';
         var enableModule =  '`enable {module}` will activate module: _standup_, _monitoring_, _reporting_.\n';
         var disableModule = '`disable {module}` stop module.\n';
-        var help = '`help` for this wondeful and useful list.\n\n';
+        var help = '`help` for this wondeful and useful list.\n';
+        var admin = '`admin {@users}` will set rights for various commands.\n\n'
         var standup = '*_Standup_*\n';
         var startStandup = '`start standup` will automatically start daily standup for your team.\n';
-        var cancel = '`cancel` cancel current standup.\n\n';
+        var cancel = '`cancel` cancel current standup.\n';
+        var schedule = '`standup time {HH:MM}` for scheduling daily standups.\n';
+        var nope = '`skip` `no` `nope` `nothing` `none` for skip question.'
+        var add = '`standup add {@users}` to invite users for daily standup.\n\n';
         var monitoring = '*_Monitoring_*\n';
         var monitor = '`monitor` will automatically start tracking current sprint and notify about any problem.\n\n';
         var reporting = '*_Reporting_*\n';
         var issues = '`sprint issues` provide list of issues in current sprint.\n';
-        var users = '`@user issues` provide list of assigned issues.\n';
+        var users = '`@user -i` provide list of assigned issues.\n';
         var issue = '`{Issue number} progress` progress about specific issue.';
 
         var json = {
-            text: title + general + status + enableModule + disableModule + help + standup + startStandup + cancel + monitoring + monitor + reporting + issues + users + issue
+            text: title + general + status + enableModule + disableModule + help + admin + standup + startStandup + cancel + schedule + nope + add + monitoring + monitor + reporting + issues + users + issue
         };
         return json;
     }
@@ -131,17 +116,37 @@ NEWSCHEMA('Response').make(function(schema) {
             text: 'This is status about enabled/disabled bot modules.',
             attachments: []
         };
+        modules.forEach(function (moduleObject) {
+            var fields = [];
+            if (moduleObject.name == 'standup') {
+                var users = '';
+                moduleObject.content.users.forEach(function(user) {
+                    users += '<@' + user.slackID + '> ';
+                });
 
-        if (modules) {
-            modules.forEach(function (moduleObject) {
-                var object = {
-                    title: moduleObject.name,
-                    text: moduleObject.description,
-                    color: moduleObject.enabled == true ? 'good' : '#DE0416'
-                }
-                json.attachments.push(object);
-            });
-        }
+                fields = [{
+                    title: 'Output Channel',
+                    value: '<#' + moduleObject.content.channel + '>',
+                    short: true
+                }, {
+                    title: 'Repeating at',
+                    value: moduleObject.content.scheduledTime,
+                    short: true
+                }, {
+                    title: 'Members',
+                    value: users.lenght ? users : 'None',
+                    short: false
+                }]
+            }
+
+            var object = {
+                title: moduleObject.name,
+                text: moduleObject.description,
+                color: moduleObject.enabled == true ? 'good' : '#DE0416',
+                fields: fields
+            }
+            json.attachments.push(object);
+        });
         return json;
     }
 
@@ -149,5 +154,31 @@ NEWSCHEMA('Response').make(function(schema) {
         if (message) {
             return message;
         }
+    }
+
+    function buildStandupResponse(options) {
+        var user = options.user;
+        var answers = options.answers;
+
+        var json = {
+            text: '<@' +user.slackID+ '> reported his standup status from *' + new Date().format('d. MMM yyyy') + '*',
+            attachments: []
+        };
+        answers.forEach(function(answer) {
+            var color = '#3EB890';
+            if (answers.indexOf(answer) == 1) {
+                color = '#E8A723';
+            } else if (answers.indexOf(answer) == 2) {
+                color = '#E01765';
+            }
+            var object = {
+                color: color,
+                title: answer.question,
+                text: answer.answer,
+            }
+            json.attachments.push(object);
+        });
+        console.log('VYSLEDOK', json);
+        return json;
     }
 });
