0a2606e6d79b9cb96abf664ea4fcd8bfa32c62ad
Maros Novák <novkmaros@gmail.com>
2017-04-04 15:14:18 +0200

    text 3

--- controllers/actions.js
+++ controllers/actions.js
@@ -1,8 +1,8 @@
-exports.install = function() {
-    F.route('/slack/receive', makeButtonAction, ['post']);
-}
-
-function makeButtonAction() {
-    var self = this;
-    console.log('Controller recieved',self.body);
-}
+// exports.install = function() {
+//     F.route('/slack/receive', makeButtonAction, ['post']);
+// }
+//
+// function makeButtonAction() {
+//     var self = this;
+//     console.log('Controller recieved',self.body);
+// }
--- models/bot.js
+++ models/bot.js
@@ -30,15 +30,23 @@ NEWSCHEMA('Bot').make(function(schema) {
             token: process.env.SLACK_API_KEY
         }).startRTM();
 
-        // model.slack.setupWebserver(3000, function(err,webserver) {
-        //     model.slack.createWebhookEndpoints(model.slack.webserver);
-        // });
+        model.slack.setupWebserver(3000, function(err,webserver) {
+            model.slack.createWebhookEndpoints(model.slack.webserver);
+
+            model.slack.createOauthEndpoints(model.slack.webserver,function(err,req,res) {
+                if (err) {
+                    res.status(500).send('ERROR: ' + err);
+                } else {
+                    res.send('Success!');
+                }
+            });
+        });
 
         model.parser.operation('initAnalyzer');
 
-        // model.slack.on('interactive_message_callback', function(bot, message) {
-        //     console.log('RECEIVED MESSAGE', message);
-        // });
+        model.slack.on('interactive_message_callback', function(bot, message) {
+            console.log('RECEIVED MESSAGE', message);
+        });
 
         model.slack.on('direct_message', function(bot, message) {
             model.parser.operation('parseMessage', message, function(err, intent) {
